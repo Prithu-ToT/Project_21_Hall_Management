5e97b149347f2b59fb07a6397013da3854eff2a2
Alamin <mdaslam6219@gmail.com>
2026-04-04 16:34:13 +0600

    removed run_app from tracking

--- run_app.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-start cmd /k "cd /d D:\Academics\2-1\Term_Project\Project_21_Hall_Management\client && npm run dev"
-
-start cmd /k "cd /d D:\Academics\2-1\Term_Project\Project_21_Hall_Management\server && npm run dev"
